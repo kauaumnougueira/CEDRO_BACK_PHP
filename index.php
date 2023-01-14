0a5fba028ae0374be44559122048da7b890ae64e
kauaumonougueira <kaua_kna@hotmail.com>
2023-01-14 21:02:25 +0044

    inicio da tela de login

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Login</title>
 </head>
 <body>
-    
+    <div id="login">
+        <form action="" method="POST">
+            <div style="margin: 20px;">
+                <img src="imagens\cedro_logo.png" alt="cedro_logo" width="200px" height="90px">
+            </div>
+            <div style="margin: 10px;">
+                <label style="margin-right: 6px;">Usuário</label>
+                <input type="text">
+            </div>
+            <div style="margin: 10px;">
+                <label style="margin-right: 18px;">Senha</label>
+                <input type="password">
+            </div>
+            <div>
+                <p><a href="https://labcedro.com.br/" target="_blank">Não consegue logar? entre em contato com os laboratórios CEDRO</a></p>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
